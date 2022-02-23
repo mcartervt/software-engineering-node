4c56bc18e068572541fbd055cff00b28e97d069f
Mark <carter.m@northeastern.edu>
2022-02-23 10:05:54 -0500

    Added Like Controller instance to server.ts

--- server.ts
+++ server.ts
@@ -5,7 +5,7 @@
  //import CourseController from "./controllers/CourseController";
  import UserController from "./controllers/UserController";
  import TuitController from "./controllers/TuitController";
- //import LikeController from "./controllers/LikeController";
+ import LikeController from "./controllers/LikeController";
  import mongoose from "mongoose";
  
 // Build the connection string
@@ -16,11 +16,8 @@ const HOST = "cluster0.16p9j.mongodb.net";
 const DB_NAME = "tuiter";
 const DB_QUERY = "retryWrites=true&w=majority";
 const connectionString = `${PROTOCOL}://${DB_USERNAME}:${DB_PASSWORD}@${HOST}/${DB_NAME}?${DB_QUERY}`;
-/* console.log(DB_USERNAME)
-console.log(DB_PASSWORD) */
- // connect to the database
+
  mongoose.connect(connectionString);
- //mongoose.connect('mongodb+srv://carterm:admin@cluster0.16p9j.mongodb.net/tuiter?retryWrites=true&w=majority');
  //mongoose.connect("mongodb://localhost:27017/tuiter");
  
  // create RESTful Web service API
@@ -36,7 +33,7 @@ console.log(DB_PASSWORD) */
  //const courseController = new CourseController(app);
  const userController = UserController.getInstance(app);
  const tuitController = TuitController.getInstance(app);
- //const likesController = LikeController.getInstance(app);
+ const likesController = LikeController.getInstance(app);
  
 
 const PORT = 4000; 
