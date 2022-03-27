266e77972cae617915a9ba890b69378e0341601e
Mark <carter.m@northeastern.edu>
2022-03-27 12:05:31 -0400

    Updated DB connection in AuthenticationService.ts

--- services/AuthenticationService.ts
+++ services/AuthenticationService.ts
@@ -3,14 +3,15 @@ import mongoose from "mongoose";
 
 const userDao: UserDao = UserDao.getInstance();
 
+
+// build the connection string
 const PROTOCOL = "mongodb+srv";
-const DB_USERNAME = "giuseppi";//process.env.DB_USERNAME;
-const DB_PASSWORD = "supersecretpassword";//process.env.DB_PASSWORD;
-const HOST = "cluster0.m8jeh.mongodb.net";
-const DB_NAME = "myFirstDatabase";
+const DB_USERNAME = process.env.DB_USERNAME;
+const DB_PASSWORD = process.env.DB_PASSWORD;
+const HOST = "cluster0.16p9j.mongodb.net";
+const DB_NAME = "tuiter";
 const DB_QUERY = "retryWrites=true&w=majority";
-const connectionString = `${PROTOCOL}://${DB_USERNAME}:${DB_PASSWORD}@${HOST}/${DB_NAME}?${DB_QUERY}`;
-// connect to the database
+const connectionString = `${PROTOCOL}://${DB_USERNAME}:${DB_PASSWORD}@${HOST}/${DB_NAME}?${DB_QUERY}`;// connect to the database
 mongoose.connect(connectionString);
 
 
